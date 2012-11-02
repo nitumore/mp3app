f1ea7431ca9928db55b940b43a40f20ad838f6c3
Nitin More <nmore2012@gmail.com>
2012-11-02 23:15:30 +0530

    mp3 playing from s3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mp3app::Application.config.secret_token = '10a572144cefc5b5431adb2384f7dc2c887a9315902a78506b3254eb97e0505fa0d900ea01624312155614fd38d7c8093b3d197345d665d8b3e9cf568d04c718'
